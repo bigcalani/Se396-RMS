d40715609795092c97b9625e70ce50687470d014
PPhKChL <ppkln@users.noreply.github.com>
2019-09-20 10:15:34 +0700

    change texts

--- configdb.php
+++ configdb.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "1234";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,'rms');
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection Database failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connection Database successfully";
 ?>
